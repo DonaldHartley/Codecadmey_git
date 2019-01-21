383ee94efe5cf8ec866aea7fcd3b68b3df61120c
Donald Hartley <42212185+DonaldHartley@users.noreply.github.com>
2019-01-21 15:35:41 -0600

    Update coded_correspondence.py

--- coded_correspondence.py
+++ coded_correspondence.py
@@ -1,1 +1,11 @@
-
+def ces_cyp_decode(text, offset):
+    alphabet = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+    decoded_text=""
+    for char in text:
+        if char not in alphabet:
+            decoded_text += char
+        elif char in alphabet:
+            index = alphabet.find(char,26,53)
+            decoded_text += alphabet[index+offset]
+    return decoded_text
+#add more logic to detect other problems
